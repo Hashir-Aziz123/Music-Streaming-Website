d8abbe4ae222c5bed9431682be2cbce87c16d317
Abdur Rafeh <abdurrafeh853@gmail.com>
2025-04-30 09:23:28 +0500

    Update schemas

--- server/db_entities.js
+++ server/db_entities.js
@@ -1,6 +1,8 @@
+
 import mongoose from "mongoose";
 
-const song_schema = new mongoose.Schema({
+const song_schema = new mongoose.Schema(
+    {
         title:
         {
             type: String,
@@ -8,12 +10,14 @@ const song_schema = new mongoose.Schema({
             required: true
         },
         artist:
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Artist",
-            required: true,
-            index: true
-        },
+        [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Artist",
+                required: true,
+                index: true
+            }
+        ],
         album:
         {
             type: mongoose.Schema.Types.ObjectId,
@@ -21,11 +25,13 @@ const song_schema = new mongoose.Schema({
             default: ""
         },
         genre:
-        {
-            type: String,
-            default: "",
-            index: true
-        },
+        [
+            {
+                type: String,
+                default: "",
+                index: true
+            }
+        ],
         duration_seconds:
         {
             type: Number,
@@ -59,152 +65,195 @@ const song_schema = new mongoose.Schema({
         description:
         {
             type: String,
-            maxlength:500,
+            maxlength: 500,
             default: ""
         }
     }
 );
 
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true
-    },
-    password_hash: {
-        type: String,
-        required: true
-    },
-    profile_picture_url: {
-        type: String,
-        default: ""
-    },
-    date_joined: {
-        type: Date,
-        default: Date.now
-    },
-    liked_songs: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Song'
-    }],
-    role: {
-        type: String,
-        required: true
+const user_schema = new mongoose.Schema(
+    {
+        username: 
+        {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: 
+        {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true
+        },
+        password_hash: 
+        {
+            type: String,
+            required: true
+        },
+        profile_picture_url: 
+        {
+            type: String,
+            default: ""
+        },
+        date_joined: 
+        {
+            type: Date,
+            default: Date.now
+        },
+        liked_songs: 
+        [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Song'
+            }
+        ]
     }
-});
+);
 
-const playlistSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    description: {
-        type: String,
-        default: ""
-    },
-    cover_image_url: {
-        type: String,
-        default: ""
-    },
-    creator: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    songs: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Song'
-    }],
-    is_public: {
-        type: Boolean,
-        default: true
+const playlist_schema = new mongoose.Schema(
+    {
+        name: 
+        {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description: 
+        {
+            type: String,
+            default: ""
+        },
+        cover_image_url: 
+        {
+            type: String,
+            default: ""
+        },
+        creator: 
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        songs: 
+        [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Song'
+            }
+        ],
+        is_public: 
+        {
+            type: Boolean,
+            default: true
+        }
+    }, 
+    {
+        timestamps: true 
     }
-}, { timestamps: true });
+);
 
-const albumSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        index: true,
-        trim: true
-    },
-    artist: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Artist',
-        required: true
-    },
-    cover_image_url: {
-        type: String,
-        default: ""
-    },
-    release_date: {
-        type: Date,
-        required: true
-    },
-    songs: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Song'
-    }],
-    description: {
-        type: String,
-        default: ""
+const album_schema = new mongoose.Schema(
+    {
+        title: 
+        {
+            type: String,
+            required: true,
+            index: true,
+            trim: true
+        },
+        artist: 
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Artist',
+            required: true
+        },
+        cover_image_url: 
+        {
+            type: String,
+            default: ""
+        },
+        release_date: 
+        {
+            type: Date,
+            required: true
+        },
+        songs: 
+        [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Song'
+            }
+        ],
+        description: 
+        {
+            type: String,
+            default: ""
+        }
+    }, 
+    { 
+        timestamps: true 
     }
-}, { timestamps: true });
+);
 
 
-const artistSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        index: true,
-        trim: true
-    },
-    bio: {
-        type: String,
-        default: ""
-    },
-    profile_picture_url: {
-        type: String,
-        default: ""
-    },
-});
-
-const listeningHistorySchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    song: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Song',
-        required: true
-    },
-    listened_at: {
-        type: Date,
-        default: Date.now
-    },
-    duration_listened: {
-        type: Number, // in seconds
-        default: 0
+const artist_schema = new mongoose.Schema(
+    {
+        name: 
+        {
+            type: String,
+            required: true,
+            index: true,
+            trim: true
+        },
+        bio: 
+        {
+            type: String,
+            default: ""
+        },
+        profile_picture_url: 
+        {
+            type: String,
+            default: ""
+        },
     }
-});
+);
 
-const ListeningHistory = mongoose.model('ListeningHistory', listeningHistorySchema);
-const Artist = mongoose.model('Artist', artistSchema);
-const Album = mongoose.model('Album', albumSchema);
-const Playlist = mongoose.model('Playlist', playlistSchema);
-const User = mongoose.model("User", userSchema);
+const listening_history_schema = new mongoose.Schema(
+    {
+        user: 
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        song: 
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Song',
+            required: true
+        },
+        listened_at: 
+        {
+            type: Date,
+            default: Date.now
+        },
+        duration_listened: 
+        {
+            type: Number, // in seconds
+            default: 0
+        }
+    }
+);
+
+const Listening_History = mongoose.model('ListeningHistory', listening_history_schema);
+const Artist = mongoose.model('Artist', artist_schema);
+const Album = mongoose.model('Album', album_schema);
+const Playlist = mongoose.model('Playlist', playlist_schema);
+const User = mongoose.model("User", user_schema);
 const Song = mongoose.model('Song', song_schema);
 
-export {Song, User, Playlist, Album, ListeningHistory, Artist};
+export {Song , User , Playlist , Album , Listening_History , Artist};
