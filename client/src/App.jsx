b7740d2c5447cdb810472409509e91b1d7bb1872
Abdur Rafeh <abdurrafeh853@gmail.com>
2025-05-11 18:48:55 +0500

    Only allow authenticated users to access home and profile sections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,34 @@ import Home from "./Pages/Home/Home.jsx";
 import Login from "./Pages/Login/Login.jsx";
 import Signup from "./Pages/Signup/Signup.jsx";
 import Profile from "./Pages/Profile/Profile.jsx";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import { useAuth } from './context/AuthContext.jsx';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const ProtectedRoute = ({ children }) => {
+    const { isAuthenticated } = useAuth();
+
+    if (!isAuthenticated) return <Navigate to='/login' />
+
+    return children;
+}
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={
+                    <ProtectedRoute>
+                        <Home />
+                    </ProtectedRoute>
+                } />
                 <Route path='/login' element={<Login />} />
                 <Route path='/signup' element={<Signup />} />
-                <Route path='/profile' element={<Profile />} />
+                <Route path='/profile' element={
+                    <ProtectedRoute>
+                        <Profile />
+                    </ProtectedRoute>
+                } />
                 <Route path="*" element={<div>Page not found</div>} />
             </Routes>
         </BrowserRouter>
